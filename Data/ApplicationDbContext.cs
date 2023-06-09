f31bdabd6eb968b255b7a79f141fbb7ba582fc3f
Arina Parfenova <78087757+KarlStrater@users.noreply.github.com>
2023-06-10 01:30:23 +0400

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using AwesomeNetwork.Configs;
+using AwesomeNetwork.Models.Users;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeNetwork.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new FriendConfiguration());
+            builder.ApplyConfiguration(new MessageConfuiguration());
+        }
+    }
+}
